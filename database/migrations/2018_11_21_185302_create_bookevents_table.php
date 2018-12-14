6bab2a0142ded32b58c9bf04d33a058e1c16a18d
Koushik Acharja <macharja625@gmail.com>
2018-12-14 09:17:17 -0800

    pre version

--- database/migrations/2018_11_21_185302_create_bookevents_table.php
+++ database/migrations/2018_11_21_185302_create_bookevents_table.php
@@ -16,14 +16,21 @@ class CreateBookeventsTable extends Migration
         Schema::create('bookevents', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title', 100);
-            $table->string('description', 150)->nullable();
-            $table->string('day', 10);
-            $table->string('hour', 10);
-            $table->string('min', 10)->nullable();
-            $table->string('location', 100);
+            $table->string('description', 3000)->nullable();
+            $table->string('day', 50);
+            $table->string('hour', 50);
+            $table->string('min', 50)->nullable();
+            $table->string('location', 200);
             $table->string('sponsors', 200)->nullable();
-            $table->string('guest', 10);
+            $table->string('guest', 150)->nullable();
             $table->string('event_type', 60)->nullable();
+            $table->string('special_requirements', 100)->nullable();
+            $table->string('describe', 3000)->nullable();
+            $table->string('catering_info', 100)->nullable();
+            $table->string('type_of_catering', 100)->nullable();
+            $table->string('special_catering', 100)->nullable();
+            $table->string('time_of_food', 60)->nullable();
+            $table->string('catered_numbers', 60)->nullable();
             $table->timestamps();
         });
     }
